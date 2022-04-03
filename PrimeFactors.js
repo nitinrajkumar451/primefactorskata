91e822e779361dfd72a8e994c1ce02597a4a6141
nitinmathie <nitinrajkumar502@gmail.com>
2022-04-03 17:06:57 +0530

    factors of 1-8 numbers

--- PrimeFactors.js
+++ PrimeFactors.js
@@ -2,7 +2,15 @@ function primeFactors(n){
     var factors= [];
     if (n>1)
     {
+        if(n%2==0)
+        {
+        factors.push(2);
+        n=n/2;
+        }
+        if(n>1)
+        {
         factors.push(n);
+        }
     }
     else
     {
--- PrimeFactors.test.js
+++ PrimeFactors.test.js
@@ -18,5 +18,35 @@ describe("Prime factors", () => {
         // assert
         expect(result).toStrictEqual([3]);
       });
+      test('Prime factors of 4 should return [2,2]', () => {
+        // arrange and act
+        var result = primeFactors(4); 
+        // assert
+        expect(result).toStrictEqual([2,2]);
+      });
+      test('Prime factors of 5 should return [5]', () => {
+        // arrange and act
+        var result = primeFactors(5); 
+        // assert
+        expect(result).toStrictEqual([5]);
+      });
+      test('Prime factors of 6 should return [2,3]', () => {
+        // arrange and act
+        var result = primeFactors(6); 
+        // assert
+        expect(result).toStrictEqual([2,3]);
+      });
+      test('Prime factors of 7 should return [7]', () => {
+        // arrange and act
+        var result = primeFactors(7); 
+        // assert
+        expect(result).toStrictEqual([7]);
+      });
+      test('Prime factors of 8 should return [2,2,2]', () => {
+        // arrange and act
+        var result = primeFactors(8); 
+        // assert
+        expect(result).toStrictEqual([2,2,2]);
+      });
    })
 
