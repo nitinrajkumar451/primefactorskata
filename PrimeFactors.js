92cc98c7c73ce4ae653af0ef461d3861016aea2a
nitinmathie <nitinrajkumar502@gmail.com>
2022-04-03 16:54:14 +0530

    for  primes 2,3

--- PrimeFactors.js
+++ PrimeFactors.js
@@ -1,4 +1,14 @@
 function primeFactors(n){
-
+    var factors= [];
+    if (n>1)
+    {
+        factors.push(n);
+    }
+    else
+    {
+        return factors;
+    }
+    
+    return factors;
 }
    module.exports = primeFactors
--- PrimeFactors.test.js
+++ PrimeFactors.test.js
@@ -6,4 +6,17 @@ describe("Prime factors", () => {
       // assert
       expect(result).toStrictEqual([]);
     });
+    test('Prime factors of 2 should return [2]', () => {
+        // arrange and act
+        var result = primeFactors(2); 
+        // assert
+        expect(result).toStrictEqual([2]);
+      });
+      test('Prime factors of 3 should return [3]', () => {
+        // arrange and act
+        var result = primeFactors(3); 
+        // assert
+        expect(result).toStrictEqual([3]);
+      });
    })
+
